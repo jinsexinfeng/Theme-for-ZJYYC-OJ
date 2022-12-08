afd240383dcebcf7ee1d42949ae00a8da7249cbc
jinsexinfeng <74101379+jinsexinfeng@users.noreply.github.com>
2022-12-08 15:41:25 +0000

    增加部分算法

--- zjyycoj.jsxf.js
+++ zjyycoj.jsxf.js
@@ -2,7 +2,7 @@
 // @name         zjyycoj by.jsxf
 // @description  1.修改oj配色
 // @namespace    https://jinsexinfeng.github.io
-// @version      0.1
+// @version      0.2
 // @author       jinsexinfeng.github.io
 // @match        https://nwanna.cn/home
 // @include      *://nwanna.cn/*
@@ -17,12 +17,14 @@ css += `
 :root {
     --ojcolor:#e83b86;
     --ojcolorb:#ee87b4;
+    --vs-ojcolor: 232,59,134;
     --white:#FFF
 }
 
 a {
     color: var(--ojcolor) ;
 }
+
 .is-primary{
     background-color: var(--ojcolorb) !important;
     color: var(--white) !important;
@@ -51,8 +53,12 @@ a {
 
 a.navbar-item:focus,
 a.navbar-item:hover{
-    background-color: var(--ojcolor) !important;
-    border-radius: 15px;
+     background-color: var(--ojcolor) !important;
+     border-radius: 15px;
+}
+.router-link-exact-active{
+     border: 2px solid var(--ojcolor) !important;
+     border-radius: 8px;
 }
 
 li.is-active > a,
@@ -73,7 +79,6 @@ li:not(.is-active) > a:hover,
    background-color: white !important;
 }
 
-
 .pagination-link.is-current{
     background-color: var(--ojcolor) !important;
     border-color:var(--ojcolor) !important;
@@ -84,26 +89,65 @@ li:not(.is-active) > a:hover,
     border-left: 5px solid var(--ojcolor) !important;
 }
 
-.router-link-exact-active{
-     border: 2px solid var(--ojcolor) !important;
-     border-radius: 8px;
-}
+
 
 .is-flex-wrap-wrap>.is-flex-grow-0>.content{
      border-radius: 8px;
 }
 
-.vs-switch{
-    --vs-color: 232,59,134 !important;
+.el-page-header__title,
+.el-icon-back{
+    color: var(--ojcolor) !important;
 }
 
 progress::-webkit-progress-value{
     background-color:var(--ojcolorb) !important;
 }
 
+
+.table-wrapper{
+    overflow: overlay;
+}
+
+.table-wrapper::-webkit-scrollbar {
+    height: 10px;
+}
+
+.vs-switch {
+    --vs-color: var(--vs-ojcolor) !important;
+}
+
+.vs-loading__load__animation__3,
+.vs-loading__load__animation__2,
+.vs-loading__load__animation__1{
+    border: 4px solid rgba(var(--vs-ojcolor),1) !important;
+}
+
 .vs-loading__load__text{
     color: var(--ojcolor) !important;
 }
+
+#rank-content>.card{
+    box-shadow: rgb(var(--vs-ojcolor)) 0px 10px 0px 0px, rgb(var(--vs-ojcolor)) 0px 0px 0px 0px !important;
+}
+
+body{
+    overflow-y: visible !important;
+}
+
+.rank>thead>tr>th{
+    background-color :rgba(var(--vs-ojcolor),0.08) !important;
+    vertical-align:bottom !important;
+}
+
+.button.is-small:not(.is-rounded) {
+    border-radius: 12px;
+}
+
+.button.is-small {
+    font-size: 0.86em;
+}
+
 
 `
 if (typeof GM_addStyle !== "undefined") {
